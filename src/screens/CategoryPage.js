491b5a27c0044effa5ccfa7ee7e611b1e98ba48d
bilenlacin <bilenlacin@gmail.com>
2021-12-22 18:42:27 +0300

    updated input area and component

--- src/screens/CategoryPage.js
+++ src/screens/CategoryPage.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import CategoryProducts from '../components/CategoryProducts';
 import Loading from '../components/Loading';
+import Products from '../components/Products';
 
 import { fetchSpecificCategory } from '../redux/productSlice';
 
@@ -26,7 +26,7 @@ const CategoryPage = () => {
           <div className='container'>
             <div className='cart-items'>
               {category.map((product) => (
-                <CategoryProducts key={product.id} product={product} />
+                <Products key={product.id} product={product} />
               ))}
             </div>
           </div>
